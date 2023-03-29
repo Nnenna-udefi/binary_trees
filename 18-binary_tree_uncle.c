c948d4cc3c246b781c9b2ff34a3e34864b6a2bc7
Nnenna Udefi <miriamudefi@gmail.com>
2023-03-29 00:16:28 -0700

    un

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 /**
  * binary_tree_uncle - function that finds the uncle of a node
@@ -12,4 +13,21 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 	return (binary_tree_sibling(node->parent));
+}
+
+
+/**
+ * binary_tree_sibling - function that finds the sibling of a node
+ * @node:  pointer to the node to find the sibling
+ * Return: node is NULL or the parent is NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
