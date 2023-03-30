7c93851d44df156c19408f673db3d9fb2f144ea5
Nnenna Udefi <miriamudefi@gmail.com>
2023-03-30 06:53:26 -0700

    queue

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -8,9 +8,8 @@ size_t binary_tree_size(const binary_tree_t *tree);
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	size_t size = 0, i;
-	int flag = 0;
-	binary_tree_t **queue = NULL;
+	size_t size, i;
+	const binary_tree_t **queue = NULL;
 
 	if (tree == NULL)
 		return (0);
@@ -21,34 +20,30 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 		return (0);
 
 	/* add the root node of the tree into the queue */
-	queue[0] = (binary_tree_t *)tree;
+	queue[0] = tree;
 
-	for (i = 0; queue[i]; i++)
+	for (i = 0; (size = 1, i < size); ++i)
 	{
 		if (queue[i]->left)
 		{
 			/* flag indicates there is a missing node in tree */
-			if (flag)
+			if (size == 1024)
 			{
 				free(queue);
 				return (0);
 			}
-			queue[++size] = queue[i]->left;
+			queue[size++] = queue[i]->left;
 		}
-		else
-			flag = 1;
 
 		if (queue[i]->right)
 		{
-			if (flag)
+			if (size == 1024)
 			{
 				free(queue);
 				return (0);
 			}
-			queue[++size] = queue[i]->right;
+			queue[size++] = queue[i]->right;
 		}
-		else
-			flag = 1;
 	}
 
 	free(queue);
